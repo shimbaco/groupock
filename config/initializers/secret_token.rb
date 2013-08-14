b74c15fbe067f1683736deaffd603ea3802254bd
Koji Shimba <bojovs@gmail.com>
2013-08-14 23:34:24 +0900

    rails new claspock --skip-bundle -d mysql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Claspock::Application.config.secret_key_base = '2dbd92e653f32213a2ecd08e08425e99926e2fb031e23884d86da4993d14547db833d0e2e77ee6df525c31b93d9c77df8ee5d3c9b43cb755b9635cc9334fde44'
